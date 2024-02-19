5fd5d7c34c9c814630fddf3fddccb3523c9e7622
JordanMaryKey <jordanmingki@gmail.com>
2024-02-20 07:29:42 +0800

    My Personal Info added

---
+++ git-python-Jordan.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 20 06:56:30 2024
+
+@author: Jordan, Mary Key N.
+"""
+
+print ("Full Name: Mary Key N. Jordan")
+print ("Address: Janangdong, Mogpog, Marinduque")
+print ("Birthday: June 30, 2003")
