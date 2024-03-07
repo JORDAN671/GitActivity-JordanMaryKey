7995c075525ed3b6a03ca53379cafc9e5fe4e435
JORDAN671 <160459143+JORDAN671@users.noreply.github.com>
2024-03-07 12:57:54 +0800

    Add files via upload

---
+++ git-python-Jordan.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 20 08:33:33 2024
+
+@author: Jordan, Mary Key N.
+"""
+
+print ("Name: Mary Key N. Jordan")
+print ("Address: Janangdong, Mogpog, Marinduque")
+print ("Birthday: June 30, 2003")
